b41d48756637746ac620ecb9e9cc566b2367c620
TakayaIto <tky.ar2013@gmail.com>
2023-01-05 23:39:29 +0900

    適当なログイン画面作成

--- firebase_auth_sample/Shared/ContentView.swift
+++ firebase_auth_sample/Shared/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputEmail: String = ""
+    @State var inputPassword: String = ""
     var body: some View {
-        Text("Hello, world!!")
-            .padding()
+        NavigationView {
+            VStack(alignment: .center) {
+                Text("Fire Base auth").font(.system(size: 36, weight: .heavy))
+                VStack(spacing: 24) {
+                    TextField("メールアドレス", text: $inputEmail).textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(maxWidth: 280)
+                    SecureField("パスワード", text: $inputPassword)
+                                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                                            .frame(maxWidth: 280)
+                }
+                .frame(height: 200)
+                Button(action: {
+                   print("Login処理")
+                },
+               label: {
+                   Text("Login")
+                       .fontWeight(.medium)
+                       .frame(minWidth: 160)
+                       .foregroundColor(.white)
+                       .padding(12)
+                       .background(Color.accentColor)
+                       .cornerRadius(8)
+               })
+            }
+        }
     }
 }
 
