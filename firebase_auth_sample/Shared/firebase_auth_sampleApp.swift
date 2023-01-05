e9b96f2b6a8e0571b12cf2a8cab3cdd517bada7b
TakayaIto <tky.ar2013@gmail.com>
2023-01-05 23:20:34 +0900

    iOS_app作成時のファイル

---
+++ firebase_auth_sample/Shared/firebase_auth_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_auth_sampleApp.swift
+//  Shared
+//
+//  Created by Takaya Ito on 2023/01/05.
+//
+
+import SwiftUI
+
+@main
+struct firebase_auth_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
